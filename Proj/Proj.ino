84f77a7e7953944939971518f01608ff60ca276a
chwchao <ccw000716@gmail.com>
2019-05-28 22:07:22 +0800

    Can't include.

--- Proj/Proj.ino
+++ Proj/Proj.ino
@@ -1,7 +1,6 @@
-#include <Adafruit_PWMServoDriver.h>  //For PCA6985 16 channel Servo controller
+#include <Adafruit_PWMServoDriver.h> //For PCA6985 16 channel Servo controller
 #include <Wire.h>
-#include <Servo.h>      //For SG90 Servo
-#include <PS2X_lib.h>   //For ps2 controller
+#include <PS2X_lib.h> //For ps2 controller
 
 //SG90 angle: 0 ~ 165 degree
 #define SG90_MIN 125
@@ -19,10 +18,7 @@
 
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 
-
-
-PS2X ps2x;  //Declare a PS2 controller
-
+PS2X ps2x; //Declare a PS2 controller
 
 int error = 0;
 
@@ -38,11 +34,9 @@ double Y = L_arm * sin((180 - L_angle) * PI / 180) - S_arm * sin(S_angle * PI / 
 double X_buf = X;
 double Y_buf = Y;
 
-
 //Variables for gripper
 double gripper_angle = 0;
 double head_angle = 86;
-
 
 //Variables for wheel
 const byte Motor1_a = 7;
@@ -52,13 +46,14 @@ const byte Motor2_b = 4;
 const byte Motor3_a = 9;
 const byte Motor3_b = 8;
 
-void setup() {
-  
+void setup()
+{
+
   Serial.begin(57600);
-  
+
   //Set up for PCA6985 16 channel Servo controller
   pwm.begin();
-  pwm.setPWMFreq(60);  // Analog servos run at ~60 Hz updates(No need to change)
+  pwm.setPWMFreq(60); // Analog servos run at ~60 Hz updates(No need to change)
 
   //Check if the ps2 controller is connected.
   error = ps2x.config_gamepad(13, 11, 10, 12, true, true); //GamePad(clock, command, attention, data, Pressures?, Rumble?)
@@ -71,31 +66,33 @@ void setup() {
   else if (error == 3)
     Serial.println("Controller refusing to enter Pressures mode, may not support it. ");
 
-  
-  Ini_Arm();  //Initialize the arm 
+  Ini_Arm(); //Initialize the arm
   Ini_Gripper();
 
   Serial.print("X:");
-    Serial.println(X);
-    Serial.print("Y:");
-    Serial.println(Y);
+  Serial.println(X);
+  Serial.print("Y:");
+  Serial.println(Y);
 }
 
-void loop() {
-  
+void loop()
+{
+
   //skip loop if no controller found
-  if (error == 1) return;
-  
+  if (error == 1)
+    return;
+
   //read controller per loop
-  ps2x.read_gamepad();     
+  ps2x.read_gamepad();
 
   /*Movement of the Gripper*/
-  if(ps2x.Analog(PSS_RY) > 130 || ps2x.Analog(PSS_RY) < 125 || ps2x.Analog(PSS_RX) > 130 || ps2x.Analog(PSS_RX) < 125){
+  if (ps2x.Analog(PSS_RY) > 130 || ps2x.Analog(PSS_RY) < 125 || ps2x.Analog(PSS_RX) > 130 || ps2x.Analog(PSS_RX) < 125)
+  {
     if (ps2x.Analog(PSS_RY) < 125)
       Y_buf += 0.25;
     else if (ps2x.Analog(PSS_RY) > 130)
       Y_buf -= 0.25;
-  
+
     if (ps2x.Analog(PSS_RX) < 125)
       X_buf -= 0.25;
     else if (ps2x.Analog(PSS_RX) > 130)
@@ -104,19 +101,18 @@ void loop() {
     Move();
   }
 
-  if(ps2x.Button(PSB_TRIANGLE))
+  if (ps2x.Button(PSB_TRIANGLE))
     Grap();
-  else if(ps2x.Button(PSB_CROSS))
+  else if (ps2x.Button(PSB_CROSS))
     Release();
 
-  if(ps2x.Button(PSB_CIRCLE))
+  if (ps2x.Button(PSB_CIRCLE))
     Head_right();
-  else if(ps2x.Button(PSB_SQUARE))
+  else if (ps2x.Button(PSB_SQUARE))
     Head_left();
 
   To_Point(14.00, 13.00);
   To_Point(14.00, 7.00);
-  
 
   delay(25);
 }
